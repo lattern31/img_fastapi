7f350fbc8dc049dd126f77fb5e52ed7f11f6c5b2
lavashik-mini <matiyukv41@gmail.com>
2024-05-30 10:46:45 +0300

    real improvements

--- src/app/common/celery_worker.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from common.settings import settings
-
-celery = Celery(
-    "tasks",
-    broker=settings.CELERY_BROKER_URI,
-    backend=settings.CELERY_RESULT_BACKEND,
-)
-celery.autodiscover_tasks(["app.auth", "app.images"])
